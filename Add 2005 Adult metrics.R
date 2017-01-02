59cb19973e1eb7981ac9db88f8ff1d7b8282e70a
11arc4 <11arc4@queensu.ca>
2017-01-02 15:33:31 -0500

    Added in capabilities to deal with males and females--all done and working properly

--- Add 2005 Adult metrics.R
+++ Add 2005 Adult metrics.R
@@ -1,8 +1,12 @@
-nestdatadir<-"~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/FINAL QC DONE"
+#Updates 2005 data to include the adult morphometrics. 
+
+
+
+nestdatadir<-"~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/"
 #We will need to do this for 2005-2016
 #up to 2001 is dealt with in the banding data, and up through 2004 has been delt with in excel
 
-filename<-"Nest Level Data 2005.csv"
+filename<-"Nest Level Data 2005 QC wo adult metrics.csv"
 nestdata<- read.csv(paste(nestdatadir, filename, sep="/"), as.is=TRUE, na.strings = c("", "NA"))
 
 
@@ -68,16 +72,72 @@ for(i in 1:length(banddata2005$Band.Number)){
         
         
       } else{
-        if(as.Date(nestdata$F.Day.measured[bandIDx], format="%Y-%m-%d")< v | 
-           as.Date(nestdata$F.Day.measured[bandIDx], format="%Y-%m-%d" )< as.Date("4/30/2005", format="%m/%d/%Y") ){
+        if(as.Date(nestdata$F.Day.measured[bandIDx], format="%Y-%m-%d" )< as.Date("4/30/2005", format="%m/%d/%Y") ){
           nestdata$F.Day.measured[bandIDx] <- as.character(v)
           message("Adult measured multiple times in banding data!")
           nestdata$F.Mass..g.[bandIDx]<-banddata2005$BIRD.mass[i]
-         #set mass 
+          #set mass 
           nestdata$F.Age[bandIDx]<-banddata2005$Age[i]            #set age
           nestdata$F.Tarsus..mm.[bandIDx]<-banddata2005$Left.Tarsus[i] #set tarsus
           nestdata$F.Wing..mm.[bandIDx]<-banddata2005$Left.Wing[i] #set wing chord (they didn't measure 9th primary)
           
+        } else{
+          if (as.Date(nestdata$F.Day.measured[bandIDx], format="%Y-%m-%d")< v){
+            next
+          } 
+        }
+        
+      }
+      
+    }
+  } #close if exists
+  
+  
+  
+} #Close for
+
+
+
+
+
+#Now we need to do the same thing for the males
+nestdata$M.Day.measured<-NA
+
+for(i in 1:length(banddata2005$Band.Number)){
+  band<-banddata2005$Band.Number[i]
+  
+  if(!is.na(band)){
+    
+    if(exists(band, M.hash_2005bands)){
+      for (bandIDx in get(band, M.hash_2005bands)){
+        d <- banddata2005$DATE[i]
+        v <- as.Date(d, format="%m/%d/%Y")
+        if(is.na(nestdata$M.Day.measured[bandIDx])){
+          
+          if(!is.na(d)){
+            nestdata$M.Day.measured[bandIDx] <- as.character(v)
+          }
+          nestdata$M.Mass..g.[bandIDx]<-banddata2005$BIRD.mass[i] #set mass
+          nestdata$M.Age[bandIDx]<-banddata2005$Age[i]            #set age
+          nestdata$M.Tarsus..mm.[bandIDx] <- banddata2005$Left.Tarsus[i] #set tarsus
+          nestdata$M.Wing..mm.[bandIDx]<-banddata2005$Left.Wing[i] #set wing chord (they didn't measure 9th primary)
+          
+          
+        } else{
+          if(as.Date(nestdata$M.Day.measured[bandIDx], format="%Y-%m-%d" )< as.Date("4/30/2005", format="%m/%d/%Y") ){
+            nestdata$M.Day.measured[bandIDx] <- as.character(v)
+            message("Adult measured multiple times in banding data!")
+            nestdata$M.Mass..g.[bandIDx]<-banddata2005$BIRD.mass[i]
+            #set mass 
+            nestdata$M.Age[bandIDx]<-banddata2005$Age[i]            #set age
+            nestdata$M.Tarsus..mm.[bandIDx]<-banddata2005$Left.Tarsus[i] #set tarsus
+            nestdata$M.Wing..mm.[bandIDx]<-banddata2005$Left.Wing[i] #set wing chord (they didn't measure 9th primary)
+            
+          } else{
+            if (as.Date(nestdata$M.Day.measured[bandIDx], format="%Y-%m-%d")< v){
+              next
+            } 
+          }
         }
       }
       
@@ -87,4 +147,5 @@ for(i in 1:length(banddata2005$Band.Number)){
   
   
 } #Close for
+
 write.csv(x=nestdata, file="~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/FINAL QC DONE/Nest Level Data 2005.csv", na="", row.names = FALSE)
