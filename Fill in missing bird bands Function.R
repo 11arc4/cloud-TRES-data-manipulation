70e31e21af41e6dd26e4ec9ed708ec029eec7c8d
11arc4 <11arc4@queensu.ca>
2017-01-03 19:59:19 -0500

    Improved functionality in the case of NAs for dates measured.

--- Fill in missing bird bands Function.R
+++ Fill in missing bird bands Function.R
@@ -21,65 +21,104 @@ fillinbandID<-function(nestdata, banddata){
       assign(site, i, hash_boxes)
     }
   }
- #now we just have to go and see if that hash ccan help us find something useful! 
-#Female
-    nestdata$F.Day.measured<-as.Date(nestdata$F.Day.measured )
+  #now we just have to go and see if that hash ccan help us find something useful! 
+  #Female
+  nestdata$F.Day.measured<-as.Date(NA )
   for (j in 1:length(nestdata$FemaleID)){
     if (is.na(nestdata$FemaleID[j])){
       NestSite <- nestdata$siteID[j]
-      if(exists(NestSite, hash_boxes)){
+      if (exists(NestSite, hash_boxes)){
         for (siteIdx in get(NestSite, hash_boxes)){
           if (banddata$Year[siteIdx]==nestdata$Year[j]){
-            if(banddata$Sex[siteIdx]=="F"){
-              if(is.na(nestdata$FemaleID[j])){
+            if(!is.na(banddata$Sex[siteIdx])){
+            if (banddata$Sex[siteIdx]=="F"){
+              if (is.na(nestdata$FemaleID[j])){
+                if(is.na(nestdata$F.Day.measured[j])){
+                  next
+                }else{
                 nestdata$FemaleID[j] <- banddata$Band.Number[siteIdx]
                 nestdata$F.Day.measured[j]<-banddata$NJDate[siteIdx]
                 message("Found a female band ID (", nestdata$FemaleID[j], ") for nest box", nestdata$siteID[j])
-              } else {
-                if (banddata$NJDate[siteIdx]<nestdata$Fledge.Fail.date[j] &
-                    abs(yday(banddata$NJDate[siteIdx]) - nestdata$First.Egg.Date[j]) <
-                    abs(yday(nestdata$F.Day.measured[j])- nestdata$First.Egg.Date[j])){
-                  nestdata$FemaleID[j] <- banddata$Band.Number[siteIdx]
-                  nestdata$F.Day.measured[j]<-banddata$NJDate[siteIdx]
-                  message("Found a female band ID (", nestdata$FemaleID[j], ") for nest box", nestdata$siteID[j])
+                }
+                } else {
+                if ( !is.na(nestdata$Fledge.Fail.date[j])){
+                  if (banddata$NJDate[siteIdx] < nestdata$Fledge.Fail.date[j] &
+                      abs(yday(banddata$NJDate[siteIdx]) - nestdata$First.Egg.Date[j]) <
+                      abs(yday(nestdata$F.Day.measured[j]) - nestdata$First.Egg.Date[j])){
+                    nestdata$FemaleID[j] <- banddata$Band.Number[siteIdx]
+                    nestdata$F.Day.measured[j] <- banddata$NJDate[siteIdx]
+                    message("Found a female band ID (", nestdata$FemaleID[j], ")
+                            for nest box", nestdata$siteID[j])
+                  }
+                }else{
+                  if (abs(yday(banddata$NJDate[siteIdx]) - nestdata$First.Egg.Date[j]) <
+                      abs(yday(nestdata$F.Day.measured[j])- nestdata$First.Egg.Date[j])){
+                    nestdata$FemaleID[j] <- banddata$Band.Number[siteIdx]
+                    nestdata$F.Day.measured[j]<-banddata$NJDate[siteIdx]
+                    message("Found a female band ID (", nestdata$FemaleID[j], ") 
+                            for nest box", nestdata$siteID[j])
+                  }
+                  
                 }
               }
             }
             
+            }
           }
         }
       }
     }
   }
-#Male
-      nestdata$M.Day.measured<-as.Date(nestdata$M.Day.measured )
+  #Male
+  nestdata$M.Day.measured<-as.Date(NA)
   for (j in 1:length(nestdata$MaleID)){
     if (is.na(nestdata$MaleID[j])){
       NestSite <- nestdata$siteID[j]
-      if(exists(NestSite, hash_boxes)){
+      if (exists(NestSite, hash_boxes)){
+        
         for (siteIdx in get(NestSite, hash_boxes)){
-          if (banddata$Year[siteIdx]==nestdata$Year[j]){
-            if(banddata$Sex[siteIdx]=="M"){
-              if(is.na(nestdata$MaleID[j])){
+          
+          if (banddata$Year[siteIdx] == nestdata$Year[j]){
+            if(!is.na(banddata$Sex[siteIdx])){
+            if (banddata$Sex[siteIdx] == "M"){
+              if ( is.na(nestdata$MaleID[j])){
+                if(is.na(nestdata$M.Day.measured[j])){
+                  next
+                  }else {
                 nestdata$MaleID[j] <- banddata$Band.Number[siteIdx]
                 nestdata$M.Day.measured[j]<-banddata$NJDate[siteIdx]
                 message("Found a Male band ID (", nestdata$MaleID[j], ") for nest box", nestdata$siteID[j])
-              } else {
-                if (banddata$NJDate[siteIdx]<nestdata$Fledge.Fail.date[j] &
-                    abs(yday(banddata$NJDate[siteIdx]) - nestdata$First.Egg.Date[j]) <
-                    abs(yday(nestdata$M.Day.measured[j])- nestdata$First.Egg.Date[j])){
-                  nestdata$MaleID[j] <- banddata$Band.Number[siteIdx]
-                  nestdata$M.Day.measured[j]<-banddata$NJDate[siteIdx]
-                  message("Found a Male band ID (", nestdata$MaleID[j], ") for nest box", nestdata$siteID[j])
+                  }
+                } else {
+                if (!is.na (nestdata$Fledge.Fail.date[j])) {
+                  
+                  if (banddata$NJDate[siteIdx] < nestdata$Fledge.Fail.date[j] &
+                      abs(yday(banddata$NJDate[siteIdx]) - nestdata$First.Egg.Date[j]) <
+                      abs(yday(nestdata$M.Day.measured[j])- nestdata$First.Egg.Date[j])){
+                    nestdata$MaleID[j] <- banddata$Band.Number[siteIdx]
+                    nestdata$M.Day.measured[j] <- banddata$NJDate[siteIdx]
+                    message("Found a Male band ID (", nestdata$MaleID[j], ") 
+                            for nest box", nestdata$siteID[j])
+                  } else{
+                    
+                    if ( abs( yday(banddata$NJDate[siteIdx] ) - nestdata$First.Egg.Date[j]) <
+                        abs( yday(nestdata$M.Day.measured[j] )- nestdata$First.Egg.Date[j])) {
+                      nestdata$MaleID[j] <- banddata$Band.Number[siteIdx]
+                      nestdata$M.Day.measured[j]<-banddata$NJDate[siteIdx]
+                      message("Found a Male band ID (", nestdata$MaleID[j], 
+                              ") for nest box", nestdata$siteID[j])
+                      
+                    }
+                  }
                 }
+                
               }
             }
-            
+            }
           }
         }
-      }
+      }  
     }
-  }  
-  
+  }
   return(nestdata)  
 }
