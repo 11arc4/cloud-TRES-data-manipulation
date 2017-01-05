2c84dc90b0ba1fc799fe471ad78e48f5a329b7f6
11arc4 <11arc4@queensu.ca>
2017-01-05 17:29:04 -0500

    Updated so to use the "Improved and Cleaned Data" folder with "1 All available adult band IDs added" as the source directory and "3 adult morphometrics added" as the output directory

--- code to add body metrics.R
+++ code to add body metrics.R
@@ -4,8 +4,8 @@ band<-read.csv("TRESBAND_75-01.csv", as.is=TRUE, na.strings = c("", "NA"))
 
 setwd("~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/R Script for adding in  adult body metrics")
 
-nestDataDir = "C:/Users/Amelia/Documents/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/FINAL QC DONE"
-resultDir = "./mangled"
+nestDataDir = "~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/Improved and Cleaned Data/1 All available adult band IDs added"
+resultDir = "~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/Improved and Cleaned Data/3 adult morphometrics added"
 
 if ( ! dir.exists(resultDir)) {
   dir.create(resultDir, recursive=TRUE)
@@ -13,11 +13,8 @@ if ( ! dir.exists(resultDir)) {
 
 nestDataFiles <- list.files(nestDataDir)
 for (fn in nestDataFiles) {
-  if (0 == length(grep("^[^0-9]+([0-9]+)\\.csv$", fn))) {
-    next
-  }
   print(paste("processing nest data", fn))
-  year = as.integer(gsub("^[^0-9]+([0-9]+)\\.csv$", "\\1", fn,perl=TRUE))
+  year = as.integer(unlist(regmatches(fn, gregexpr("[[:digit:]]+", fn))))
   if ( year < 2000) {
     year = year - 1900
   }
