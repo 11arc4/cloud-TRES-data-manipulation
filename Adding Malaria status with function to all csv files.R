235513ad49c128b2b73a9fb73e25beac30e2188e
11arc4 <11arc4@queensu.ca>
2017-01-06 09:19:29 -0500

    Updated the code to use the Improved and Cleaned Data folder--- source is "3 adult morphometrics added" and sink is "4 malaria status added"

--- Adding Malaria status with function to all csv files.R
+++ Adding Malaria status with function to all csv files.R
@@ -1,6 +1,6 @@
-
+sourcedir<-"~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/Improved and Cleaned Data/3 adult morphometrics added"
+sinkdir<- "~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/Improved and Cleaned Data/4 malaria status added"
 malaria<-read.csv("~/Masters Thesis Project/Tree Swallow Data/TRES data/Data Sets I really need to ADD to my current dataset/Individual malaria status.csv",as.is=TRUE, na.strings = c("", "NA"))
-setwd("~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/R Script for adding in  adult body metrics/Malaria and Adult metrics added")
 
 #making the has/lookupkey
 hash_malaria <- new.env(hash=TRUE, parent=emptyenv(), size=length(malaria$ID))
@@ -12,13 +12,14 @@ for (i in 1:length(malaria$ID)){
 }
 
 #making the list of data files we'd like to look at
-nestDataFiles <- list.files("~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/R Script for adding in  adult body metrics/mangled")
+#ls(hash_malaria, all.names = TRUE)
+nestDataFiles <- list.files(sourcedir)
 
 
 #looping through all of the files to make a better version of each of them
 for (i in 1:length(nestDataFiles)) {
   
-  nestdata <- read.csv(paste("~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/R Script for adding in  adult body metrics/mangled/", nestDataFiles[i], sep=""), as.is=TRUE, na.strings = c("NA", ""))  
+  nestdata <- read.csv(paste(sourcedir, nestDataFiles[i], sep="/"), as.is=TRUE, na.strings = c("NA", ""))  
   nestdata$F.Malaria.Status <- rep(NA, nrow(nestdata))
   nestdata$F.blooddate <- rep(NA, nrow(nestdata))
   
@@ -55,7 +56,8 @@ for (i in 1:length(nestDataFiles)) {
     } # if
   } # for
   
- write.csv(x=nestdata, file=paste("Nest", nestdata$Year [2], "with malaria and adult.csv", sep=" "),
+ write.csv(x=nestdata, file=
+             paste(sinkdir, "/Nest data ", nestdata$Year [2], " with malaria and adult.csv", sep=""),
            row.names=FALSE , na="")
 }
 #write the csv files into a new location
