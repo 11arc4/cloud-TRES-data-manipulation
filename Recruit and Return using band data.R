ab002e23688a621a84bed7c69bf7b12dbf3c14ff
11arc4 <11arc4@queensu.ca>
2017-01-10 15:54:08 -0500

    ammended to include the AssertReturnStatus_from BandData function

--- Recruit and Return using band data.R
+++ Recruit and Return using band data.R
@@ -106,6 +106,8 @@ for(a in 1:length(nestDataFileList)){
   message("proccessing nestdata from ", year)
   nestdata <- AssignReturnStatus_fromnestdata(nestdata, year, globalBirdDataHash)
   globalBirdDataHash <- buildBirdDataHash(nestdata, year, globalBirdDataHash)
+  AssertReturnStatus_fromBandData(nestdata = nestdata, hash_allbirds = hash_allbirds, band = band)
+  
   filename <- paste("Nest update w recruit ", nestdata$Year[1], ".csv"  ,sep="")
   write.csv(x=nestdata, file=filename, na="", row.names=FALSE)
   message("updated nest data from ", year, " with recruitment status")
