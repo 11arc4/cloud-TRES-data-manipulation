f3aee6a78391db2f7fcdab5ca0b0f78d99c9c147
11arc4 <11arc4@queensu.ca>
2017-01-17 16:18:35 -0500

    move this into the tree swallow classes file--can't deal wwith methods and classes seperately because of the circular nature.

--- RC Class Data Structure Creation Functions.R
+++ RC Class Data Structure Creation Functions.R
@@ -1,72 +1,77 @@
+library(assertthat)
 
-#First we need to set up a function that will add all the data from a nest into the 
-NestData$methods(
-  initialize = function(year, lineNumber, date = NA, male = NA,
-                        female=NA, nestlings = list()) {
-    #or maybe pass the 'nestData' file, line number, and year - and we figure it out
-    .self$date <<- date
-    .self$year <<- year
-    .self$lineNumber <<- lineNumber
-    .self$male <<- male
-    .self$female <<- female
-    .self$nestlings <<- nestlings
-    
-    #male$addNesting(.self)
-  },
-  configureFromNestDataFile = function(nestDataFrame, line) {
-    #lineNumber <<- line 
-    #Could add in lineNumber reference but I don't think it's really necessary
-    #since we are now including all the dates and stuff
-    
-    year <<- nestDataFrame$Year[line]
-    
-    #Here are the bird parents
-    if (! is.na(nestDataFrame$femaleID[line]) ) {
-     female <<- nestDataFrame$femaleID[line]
-      # find the female from its ID and enter it here
-    }
-    if (!is.na(nestDataFrame$maleID[line])){
-      male <<- nestDataFrame$maleID[line]
-    }
-    
-    #Need to add in a list of nestlings--create that before
-    nestlings <<- nestlingbandIDlist  #Nestlings needs to be made into a list and entered!
-    
-    #Here are all the important dates
-    if (!is.na(nestDataFrame$First.Egg.Date[line])){
-      FirstEggDate <<- nestDataFrame$First.Egg.Date[line]
-    }
-    if(!is.na(nestDataFrame$Incubation.Date[line])){
-      LastEggDate<<-nestDataFrame$Incubation.Date[line]
-    }
-    
-    if(!is.na(nestDataFrame$Hatch.Date[line])){
-      HatchDate <<- nestDataFrame$Hatch.Date[line]
-    }
-    if(!is.na(nestDataFrame$Fledge.Fail.date[line])){
-      FledgeDate <<- nestDataFrame$Fledge.Fail.date[line]
-    }
 
-    #Breeding success measurements
-    if(!is.na(nestdataframe$Clutch.Size[line])){
-      ClutchSize <<- nestdataframe$Clutch.Size[line]
-    }
-    if(!is.na(nestdataframe$Hatch.Size[line])){
-      HatchSize <<- nestdataframe$Hatch.Size[line]
-    }
-    if(!is.na(nestdataframe$Fledge.Size[line])){
-      FledgeSize <<- nestdataframe$Fledge.Size[line]
-    }
-    if(!is.na(nestdataframe$Fledge.Size[line])){
-      ReasonforFailure <<- nestdataframe$Why.fail.[line]
-    }
-    
-  },
-  addNestling = function(n) {
-    # should probably assert that the type of 'n' is "TreeSwallow"
-    append(nestlings, n)
+TreeSwallowPointer$methods(
+  initialize = function (bandID, bird_hash){
+    .self$bandID <<- bandID
+    .self$bird_hash <<- bird_hash
+  }, 
+  getbird= function() {
+    get(bandID, bird_hash )
   }
 )
+
+
+
+configureFromNestDataFile = function(nestDataFrame, line, bird_hash) {
+  #lineNumber <<- line 
+  #Could add in lineNumber reference but I don't think it's really necessary
+  #since we are now including all the dates and stuff
+  assert_that(year == nestDataFrame$Year[line])
+  
+  #Here are the bird parents
+  if (! is.na(nestDataFrame$femaleID[line]) ) {
+    
+    femaleID <- nestDataFrame$femaleID[line]
+    if(exists(femaleID, bird_hash)) {
+      female <<- TreeSwallowPointer( femaleID, bird_hash)
+    } else {
+      bird<- 
+        assign( )
+    }
+    # find the female from its ID and enter it here
+  }
+  if (!is.na(nestDataFrame$maleID[line])){
+    male <<- nestDataFrame$maleID[line]
+  }
+  
+  
+  #Here are all the important dates
+  if (!is.na(nestDataFrame$First.Egg.Date[line])){
+    FirstEggDate <<- nestDataFrame$First.Egg.Date[line]
+  }
+  if(!is.na(nestDataFrame$Incubation.Date[line])){
+    LastEggDate<<-nestDataFrame$Incubation.Date[line]
+  }
+  
+  if(!is.na(nestDataFrame$Hatch.Date[line])){
+    HatchDate <<- nestDataFrame$Hatch.Date[line]
+  }
+  if(!is.na(nestDataFrame$Fledge.Fail.date[line])){
+    FledgeDate <<- nestDataFrame$Fledge.Fail.date[line]
+  }
+  
+  #Breeding success measurements
+  if(!is.na(nestdataframe$Clutch.Size[line])){
+    ClutchSize <<- nestdataframe$Clutch.Size[line]
+  }
+  if(!is.na(nestdataframe$Hatch.Size[line])){
+    HatchSize <<- nestdataframe$Hatch.Size[line]
+  }
+  if(!is.na(nestdataframe$Fledge.Size[line])){
+    FledgeSize <<- nestdataframe$Fledge.Size[line]
+  }
+  if(!is.na(nestdataframe$Fledge.Size[line])){
+    ReasonforFailure <<- nestdataframe$Why.fail.[line]
+  }
+  
+},
+addNestling = function(n) {
+  # should probably assert that the type of 'n' is "TreeSwallow"
+  append(nestlings, n)
+}
+)
+
 
 
 
@@ -76,10 +81,10 @@ NestData$methods(
 
 TreeSwallow$methods(
   initialize = function(id, nestlingRecord=NA) {
-    bandId <<- id
-    nestling <<- nestlingRecord
-    nestList <<- list()
-    observations <<- list()
+    .self$bandId <<- id
+    .self$nestling <<- nestlingRecord
+    .sefl$nestList <<- list()
+    .self$observations <<- list()
   },
   addNesting = function(nest) {
     append(nestList, nest)
