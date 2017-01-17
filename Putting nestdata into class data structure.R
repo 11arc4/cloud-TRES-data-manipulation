09fef1ce9fd00a1815faeb1e570caa8ac31f5fe5
11arc4 <11arc4@queensu.ca>
2017-01-17 16:15:56 -0500

    Initial commit to create tree swallows and nests based on a set of nest data. Getting an error message that I'm applying is.na() to an S4 object and that you can't do that.... unsure how to proceed

--- Putting nestdata into class data structure.R
+++ Putting nestdata into class data structure.R
@@ -6,7 +6,7 @@ listfiles <- list.files(nestdatadir)
 
 for (filename in listfiles){
   nestdata <- read.csv ( paste (nestdatadir, filename, sep="/"))
-  for (i in 1: length(nestdata$Year))
+  for (i in 1: length(nestdata$Year)){
     #Need to create the list of nestling band IDs to feed into the function
     nestband<-c("band.1", "band.2", "band.3", "band.4", "band.5", 
                 "band.6", "band.7", "band.8", "band.9", "band.10")
@@ -14,4 +14,5 @@ for (filename in listfiles){
     nestlingbandIDlist<-append(nestdata[i, nestband[a]], nestlingbandIDlist)
   }
   configureFromNestDataFile (nestDataFrame =nestdata, line=i )
+  }
 }
