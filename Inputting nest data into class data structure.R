f55faa0919e9ccdfd4dc507e548b60493cc45144
henrygcoxx <henrygcox@comcast.net>
2017-01-21 15:47:53 -0500

    use generic EnvPointer for optional TreeSwallow, Nest, and Nesting pointers.

--- Inputting nest data into class data structure.R
+++ Inputting nest data into class data structure.R
@@ -12,28 +12,32 @@ nestlings_hash <- new.env(hash=TRUE, parent=emptyenv())
 
 year<- nestdata$Year[1]
 for (i in 1: length(nestdata$Year)){
-  nestID <- paste (as.character(year), nestdata$BoxID[i], sep="-")  #This is the unique 
+  nestID <- paste (as.character(year), nestdata$BoxID[i], sep="-")  #This is the unique
   nest <- Nest(year=year, siteID=nestdata$siteID[i] )
-  
+
   #Need to create (or append) sightings of the parents as TreeSwallows
-  if (! is.na(nestdata$FemaleID[i]) ) {
-    
-    femaleID <- as.character(nestdata$FemaleID[i])
-    
-    if(exists(femaleID, bird_hash)) {
-      # find the female from its ID and enter it here
-      nest$femaleID <- TreeSwallowPointer( femaleID, bird_hash)
-      
+  femaleID <- as.character(nestdata$FemaleID[i])
+  if (! is.na(femaleID) {
+
+    # look for this bird is the globalSingleton
+    bird <- globalSingleton$findBird(femaleId)
+    if (is.na(bird)) {
+      # this is the first time we have seen this female...buid a TreeSwallow for it...
+      bird <- TreeSwallow(bandID=femaleID, sex="F")
     } else {
-      #need to create a new TreeSwallow entry for that bird, put it into the hash, and  
-      TRESentry <- TreeSwallow(bandID=femaleID)
-      assign(x=femaleID, value=TRESentry, bird_hash)
-      nest$femaleID <- TreeSwallowPointer(femaleID, bird_hash)
+      # we saw this one before...check that the sex is the same as the last time - or maybe this was a nestling
+      #   and we didn't know the sex
+      if ("N" == bird$sex()) {
+        bird$sex("F")
+      } else if ("F" != bird$sex()) {
+        message("Thats odd.. female ", femaleID, " was male last time.")
+      }
     }
-  } else {
-    nest$femaleID <- TreeSwallowPointer(bandID= NA_character_, bird_hash)
+    bird$addNest(nest)
   }
-  
+  nest$femaleID <- EnvPointer(femaleID, dataSingleton$birds)
+
+
   if (!is.na(nestdata$MaleID[i])){
     maleID <- as.character(nestdata$MaleID[i])
     if(exists(maleID, bird_hash)){
@@ -49,8 +53,10 @@ for (i in 1: length(nestdata$Year)){
   } else {
     nest$maleID <- TreeSwallowPointer(bandID= NA_character_, bird_hash)
   }
-  
-  
+
+  # call a method on the nest to populate the rest of the data
+  #nest$populateMesaurements(nestdata, rownumber)
+
   #Here are all the important dates
   if (!is.na(nestdata$First.Egg.Date[i])){
     nest$firstEggDate <- nestdata$First.Egg.Date[i]
@@ -58,14 +64,14 @@ for (i in 1: length(nestdata$Year)){
   if(!is.na(nestdata$Incubation.Date[i])){
     nest$lastEggDate <- nestdata$Incubation.Date[i]
   }
-  
+
   if(!is.na(nestdata$Hatch.Date[i])){
     nest$hatchDate <- nestdata$Hatch.Date[i]
   }
   if(!is.na(nestdata$Fledge.Fail.date[i])){
     nest$fledgeDate <- nestdata$Fledge.Fail.date[i]
   }
-  
+
   #Breeding success measurements
   if(!is.na(nestdata$Clutch.Size[i])){
     nest$clutchSize <- nestdata$Clutch.Size[i]
@@ -77,12 +83,12 @@ for (i in 1: length(nestdata$Year)){
     nest$fledgeSize <- nestdata$Fledge.Size[i]
   }
   if(!is.na(nestdata$Why.fail.[i])){
-    nest$reasonforFailure - as.character(nestdata$Why.fail.[i]) 
-  } 
+    nest$reasonforFailure <- as.character(nestdata$Why.fail.[i])
+  }
   if(!is.na(nestdata$renest.status[i])){
     nest$renestStatus <- as.character(nestdata$renest.status[i])
-  } 
-  
+  }
+
   #Need to make all the nestlings, and also need to add them to the data
   for (j in 1:10){
     bandoptions<-c("band.1","band.2", "band.3",
@@ -93,17 +99,17 @@ for (i in 1: length(nestdata$Year)){
     }
     if(exists(chick$fromNest, nestlings_hash)){
       assign(x=chick$fromNest, values=append(x=get(chick$fromNest, nestlings_hash) , values=chick), nestlings_hash)
-      
+
     } else{
     assign(x=chick$fromNest, values=chick, nestlings_hash)
     }
   }
   nest$nestlings <- get(nestID, nestlings_hash)
-  
+
   #put the new nest into the hash
-  assign(x=nestID, value=nest, envir=nest_hash) 
-  
-  
+  assign(x=nestID, value=nest, envir=nest_hash)
+
+
 }
 
 
