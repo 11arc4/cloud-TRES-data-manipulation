f549fe718759f6e5637a2e581d277664285d551b
11arc4 <11arc4@queensu.ca>
2017-02-07 14:52:29 -0500

    Using the Nest globalData only (all that's necessary for the nestling growth rate work)

--- Growth Rate Calculations.R
+++ Growth Rate Calculations.R
@@ -3,7 +3,7 @@
 AllNestlings <- as.list(globalData$nestlings)
 
 
-for (nestling in as.list(globalData$nestlings)){
+for (nestling in as.list(globalDataNest$nestlings)){
   growthrate <- nestling$calcGrowthRate()
   if (is.na(growthrate)){
     message("growthrate is NA for nestling ", nestling$nestlingCode)
