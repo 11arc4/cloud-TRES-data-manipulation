31a312d3a34590170f8e09edbc921ae8518aaecf
11arc4 <11arc4@queensu.ca>
2017-02-07 17:16:33 -0500

    Added code to write out a CSV file (to the desktop) including year, nest, hatch date, hatchsize, fledge size and growthrate (by mass) for use in modeling! YAY this is going really well

--- Growth Rate Calculations.R
+++ Growth Rate Calculations.R
@@ -2,13 +2,14 @@
 
 AllNestlings <- as.list(globalData$nestlings)
 
-NstgGrowth <- data.frame(nestID = rep(NA,10),
-                         year= rep(NA, 10),
-                         hatchdate=rep(NA, 10), 
-                         hatchsize = rep(NA, 10), 
-                         fledgesize = rep(NA, 10)
-                         
-) 
+nestID = c()
+nestyear= c()
+hatchdate=c()
+hatchsize = c()
+fledgesize = c()
+massgrowth =c()
+nest = c()
+
 
 for (nestling in as.list(globalDataNest$nestlings)){
   growthrate <- nestling$calcGrowthRate()
@@ -18,24 +19,38 @@ for (nestling in as.list(globalDataNest$nestlings)){
   nestling$addGrowthRateMass(growthrate)
   #if we were able to calculate growth rates then we want to use it in modeling and the relevent data needs to be pulled out!
   if(!is.na(growthrate)){
+    message("made it")
     nestID <- nestling$fromNest$m_key
     fromNest <- get(nestID, globalDataNest$nests)
     
-    NstgGrowth$year [ length(NstgGrowth$year) + 1] <- fromNest$year
-    NstgGrowth$hatchdate [ length(NstgGrowth$hatchdate) + 1] <- fromNest$hatchDate
-    NstgGrowth$hatchdsize [ length(NstgGrowth$hatchsize) + 1] <- fromNest$hatchSize
-    NstgGrowth$year [ length(NstgGrowth$year) +1 ] <- fromNest$year
-    NstgGrowth$fledgesize [ length(NstgGrowth$fledgesize) + 1] <- fromNest$fledgeSize
-    
+    nestyear [ length(nestyear) + 1] <- fromNest$year
+    hatchdate [ length(hatchdate) + 1] <- fromNest$hatchDate
+    hatchsize [ length(hatchsize) + 1] <- fromNest$hatchSize
+    year [ length(year) +1 ] <- fromNest$year
+    fledgesize [ length(fledgesize) + 1] <- fromNest$fledgeSize
+    massgrowth [ length(massgrowth) + 1] <- growthrate
+    nest [length(nest) + 1] <- nestID
   }
   
 }
 
+NstgGrowth <- cbind(as.integer(nestyear), 
+                    as.character(nest), 
+                    as.integer(hatchdate), 
+                    as.integer(hatchsize), 
+                    as.integer(fledgesize), 
+                    as.numeric(massgrowth))
+colnames(NstgGrowth) <- c("year", "nest", "hatchdate", "hatchsize", "fledgesize", "growthrate_mass")
 
-#NICE THIS WORKS 
+#NICE THIS WORKS!
 
 
-#OK to actually use this data I will need to pull the data out and put it into a data frame!
+#OK to now I can toss this data out onto a csv file and read it in for analysis!
+
+outputdir <-"~/Amelia TRES data 1975-2016/Extracted Data for Analysis"
+
+write.csv(NstgGrowth, file= paste (outputdir, "Nestling Growth Rate Modeling Data.csv", sep= "/"), row.names = FALSE, na= "")
+
 
 
 
