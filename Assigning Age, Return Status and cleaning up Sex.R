d6234d212d032f27a92f01bde5ce53a6aebe15f4
11arc4 <11arc4@queensu.ca>
2017-02-13 19:22:07 -0500

    Added more statements to change ages into consistent format. Added check to make sure there aren't duplicate years Improved age calculation for slow return birds

--- Assigning Age, Return Status and cleaning up Sex.R
+++ Assigning Age, Return Status and cleaning up Sex.R
@@ -1,25 +1,23 @@
 #Fixing ages, fixing sex where appropriate and adding return status
-
-library(plyr)
-exactAges <- c("HY", "SY", "3Y", "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "10Y", "11Y" )
-estimateAges <- c("AHY", "ASY", "A3Y", "A4Y", "A5Y", "A6Y", "A7Y", "A8Y", "A9Y", "A10Y", "A11Y")
+library(beepr)
+exactAges <- c("HY", "SY", "3Y", "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "10Y", "11Y", "12Y", "13Y" )
+estimateAges <- c("AHY", "ASY", "A3Y", "A4Y", "A5Y", "A6Y", "A7Y", "A8Y", "A9Y", "A10Y", "A11Y", "A12Y")
 
 
 #AllBirds <- as.list(globalData$birds)
 
 for (bird in as.list(globalData$birds)){
-  message("updating Tree Swallow ", bird$bandID )
+  #message("updating Tree Swallow ", bird$bandID )
   #need to sort the yearsSeen based on year (that way we can just run though
   #that list and it will all be wonderfully sorted and I will have an easy way
   #to assign return and age)
-  #NEED TO SORT THIS YEARS SEEN LIST BY YEAR! Probably just fine as is but should double check
-
+  
   
   if(bird$yearsSeen$length>1){
-   message("Sorting years")
-   l2 <- bird$yearsSeen$as.list()
-   l3 <- l2[order(sapply(l2, function(v) { v$year} ))]
-   bird$yearsSeen$replaceList(l3)
+    #message("Sorting years")
+    l2 <- bird$yearsSeen$as.list()
+    l3 <- l2[order(sapply(l2, function(v) { v$year} ))]
+    bird$yearsSeen$replaceList(l3)
   }
   #create a vector of sexes 
   sexes <- rep(NA_character_, 20)
@@ -38,20 +36,43 @@ for (bird in as.list(globalData$birds)){
         #year age stays the same!
         
         #QUick fixes to check to see that everyone is consistent about how they write ages
-        if (!is.na(year$age)){
-          if( year$age=="2Y") {
-            year$age <- "SY"
-          }
-          if(year$age == "A2Y"){
-            year$age <- "ASY"
-          }
-          if(length(year$age)==0){
-            year$age <- "AHY"
+        if (length(year$age)!=0 ){
+          if(!is.na(year$age)){
+            
+            if( year$age=="2Y") {
+              year$age <- "SY"
+            }
+            if(year$age == "A2Y"){
+              year$age <- "ASY"
+            }
+            if(year$age=="AFY"){
+              year$age ="A5Y"
+            }
+            if(year$age=="ASYR"){
+              year$age = "ASY"
+            }
+            if(year$age=="A1Y"){
+              year$age="AHY"
+            }
+            if(year$age=="TRES"){
+              year$age = "AHY"
+            }
+            if(year$age=="SYN"){
+              year$age= "SY"
+            }
+            if(year$age=="SY?"){
+              year$age="SY"
+            }
+            
+          } else {
+            message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY")
+            year$age<- "AHY"
           }
         } else {
           message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY")
           year$age<- "AHY"
         }
+        
       } else {
         year$returnStatus <- "Nestling"
         year$age <- "HY"
@@ -64,26 +85,35 @@ for (bird in as.list(globalData$birds)){
         } else {
           year$returnStatus <- "Return"
           agevec <- substring (prev$age, first=1, last=1)
-          if(agevec == "A"){
-            year$age <- estimateAges[which(estimateAges==prev$age)+1]
-          } else {
-            year$age <- exactAges[which(exactAges==prev$age)+1]
+          if(agevec==" "){
+            agevec <- substring (prev$age, first=2, last=2)
+            AGE<- prev$age
+            prev$age <- substring(AGE, first=2, last=5)
           }
-          
+          if(agevec == "A"){
+            year$age <- estimateAges[which(estimateAges==prev$age)+(year$year-prev$year)]
+          } else {
+            year$age <- exactAges[which(exactAges==prev$age)+(year$year-prev$year)]
+          }
         }
+        
         
       } else {
         year$returnStatus <- "Return"
         agevec <- substring (prev$age, first=1, last=1)
         if(agevec == "A"){
-          year$age <- estimateAges[which(estimateAges==prev$age)+1]
+          year$age <- estimateAges[which(estimateAges==prev$age)+(year$year-prev$year)]
         } else {
-          year$age <- exactAges[which(exactAges==prev$age)+1]
+          year$age <- exactAges[which(exactAges==prev$age)+(year$year-prev$year)]
         }
       }
     }
     #Setting this iteration to be the previous year's info for the next iteration
+    if(is.na(year$age)){
+     # message("Check year$age")
+    }
     prev <- year
+    
   }
   sexes <- na.omit(sexes)[[1]]
   if (nlevels (as.factor (sexes))==1) {
@@ -96,9 +126,13 @@ for (bird in as.list(globalData$birds)){
     #if sex is not in agreement
     if (timesF > timesM){
       bird$sex <- "F"
+      message("bird", bird$bandID,  "sex inconsistant-> changed to Female", sep=" ")
+      
     } else {
       if (timesM > timesF){
         bird$sex <- "M"
+        message("bird", bird$bandID,  "sex inconsistant-> changed to Male", sep=" ")
+        
       } else {
         #if the bird is seen equally as a male and as a female then I have no
         #idea what sex it is and we should put it in as an unknown bird. This
@@ -106,11 +140,14 @@ for (bird in as.list(globalData$birds)){
         #partner but that's probably for the best in case the bird is actually
         #polygamous!
         bird$sex <- "U"
+        message("bird", bird$bandID,  "sex inconsistant-> changed to unknown", sep=" ")
       }
     }
   }
-  
+ if (length(bird$viewYears()) > nlevels(as.factor(bird$viewYears()))){
+   message("Added too many versions of yearSeen in bird ", bird$bandID)
+ }
 }
 
-
+beep(1)
 
