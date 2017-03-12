97f4e5a651095822c5860955caf86746eb7c00b7
11arc4 <11arc4@queensu.ca>
2017-03-12 10:46:53 -0400

    fixed cleaning up ages

--- Assigning Age, Return Status and cleaning up Sex.R
+++ Assigning Age, Return Status and cleaning up Sex.R
@@ -25,11 +25,9 @@ for (bird in as.list(globalData$birds)){
   t=0
   for (year in bird$yearsSeen$as.list()){
     t=t+1
-    if (length(year$sex) >0){
-      sexes[length(sexes)+1] <- year$sex
-    } else {
-      sexes[t] <- "U"
-    }
+  
+    sexes[t] <- year$sex
+    
     if (t==1){
       if (is.na (year$hatchNest$m_key)){
         year$returnStatus <- "New"
@@ -85,11 +83,11 @@ for (bird in as.list(globalData$birds)){
             
             
           } else {
-            message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY")
+            message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY", sep=" ")
             year$age<- "AHY"
           }
         } else {
-          message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY")
+          message("Bird", bird$bandID, "from", year$year,  "has unknown age. Entered here as AHY", sep= " ")
           year$age<- "AHY"
         }
         
@@ -135,13 +133,17 @@ for (bird in as.list(globalData$birds)){
     prev <- year
     
   }
-  sexes <- na.omit(sexes)[[1]]
+  sexes <- sexes[1:t]
   if (nlevels (as.factor (sexes))==1) {
     #If there is only one level (ie it's Male or Female, but not both then we're all set and can assign it)
+    
+    
+    
+    ######THIS IS THE SEX ISSUE
     bird$sex <- sexes[1]
   } else {
-    timesF <- length(sexes[sexes=="F"])
-    timesM <- length(sexes[sexes=="M"])
+    timesF <- length(which(sexes=="F"))
+    timesM <- length(which(sexes=="M"))
     
     #if sex is not in agreement
     if (timesF > timesM){
@@ -151,7 +153,7 @@ for (bird in as.list(globalData$birds)){
     } else {
       if (timesM > timesF){
         bird$sex <- "M"
-        message("bird", bird$bandID,  "sex inconsistant-> changed to Male", sep=" ")
+        message("bird ", bird$bandID,  " sex inconsistant-> changed to Male", sep=" ")
         
       } else {
         #if the bird is seen equally as a male and as a female then I have no
@@ -160,7 +162,7 @@ for (bird in as.list(globalData$birds)){
         #partner but that's probably for the best in case the bird is actually
         #polygamous!
         bird$sex <- "U"
-        message("bird", bird$bandID,  "sex inconsistant-> changed to unknown", sep=" ")
+        message("bird ", bird$bandID,  " sex inconsistant-> changed to unknown", sep=" ")
       }
     }
   }
